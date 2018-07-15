65695a80555bf8dfcb3f6c4783905931226cd44a
Andrius Mudinas <keistokas@gmail.com>
2018-07-15 17:57:37 +0100

    Adding streaming factory

---
+++ src/Wikiled.Common.Net/Client/StreamApiClientFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Wikiled.Common.Net.Client
+{
+    public class StreamApiClientFactory : IStreamApiClientFactory
+    {
+        private readonly ILogger<StreamApiClient> logger;
+
+        private readonly Uri url;
+
+        private readonly HttpClient client;
+
+        public StreamApiClientFactory(ILogger<StreamApiClient> logger, HttpClient client, Uri url)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.url = url ?? throw new ArgumentNullException(nameof(url));
+        }
+
+        public StreamApiClientFactory(ILogger<StreamApiClient> logger, Uri url)
+            : this(logger, new HttpClient { Timeout = TimeSpan.FromMinutes(20) }, url)
+        {
+        }
+
+        public IStreamApiClient Contruct()
+        {
+            return new StreamApiClient(client, url, logger);
+        }
+    }
+}
